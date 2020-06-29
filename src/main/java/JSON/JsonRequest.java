f65593e3bb1e84ff1b93741d3a4739944f63056f
Theo <stoicatheo@gmail.com>
2020-06-29 13:09:54 +0300

    BS-7 client make request menu functional

---
+++ src/main/java/JSON/JsonRequest.java
@@ -0,0 +1,49 @@
+package JSON;
+
+public class JsonRequest
+{
+    private String requestSender;
+    private String requestDomain;
+    private String requestDetails;
+    private String reqeustTitle;
+
+    public String getRequestSender()
+    {
+        return requestSender;
+    }
+
+    public void setRequestSender(String requestSender)
+    {
+        this.requestSender = requestSender;
+    }
+
+    public String getRequestDomain()
+    {
+        return requestDomain;
+    }
+
+    public void setRequestDomain(String requestDomain)
+    {
+        this.requestDomain = requestDomain;
+    }
+
+    public String getRequestDetails()
+    {
+        return requestDetails;
+    }
+
+    public void setRequestDetails(String requestDetails)
+    {
+        this.requestDetails = requestDetails;
+    }
+
+    public String getReqeustTitle()
+    {
+        return reqeustTitle;
+    }
+
+    public void setReqeustTitle(String reqeustTitle)
+    {
+        this.reqeustTitle = reqeustTitle;
+    }
+}
---
+++ src/main/java/misc/Request.java
@@ -0,0 +1,78 @@
+package misc;
+
+import JSON.JsonController;
+import JSON.JsonRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Request
+{
+    private static List<JsonRequest> listRequest;
+    private static String path = "data/clientRequests/requests.json";
+
+    public static ArrayList<String> getStrListSender()
+    {
+        ArrayList<String> ret = new ArrayList<String>();
+        for(JsonRequest x:listRequest)
+        {
+            ret.add(x.getRequestSender());
+        }
+        return ret;
+    }
+
+    public static ArrayList<String> getStrListDomain()
+    {
+        ArrayList<String> ret = new ArrayList<String>();
+        for(JsonRequest x:listRequest)
+        {
+            ret.add(x.getRequestDomain());
+        }
+        return ret;
+    }
+
+    private static JsonRequest getRequest(String domain,String title)
+    {
+        return listRequest.stream().filter(dm -> domain.equals(dm.getRequestDomain()) && title.equals(dm.getReqeustTitle())).findAny().orElse(null);
+    }
+
+    public static int getIndex(String domain, String title)
+    {
+        return listRequest.indexOf(getRequest(domain,title));
+    }
+
+    public static void deleteElement(String domain,String title)
+    {
+        int pos = getIndex(domain,title);
+        if(pos != -1)
+        {
+            listRequest.remove(pos);
+            JsonController.writeArrayRequest(path, listRequest);
+        }
+    }
+
+    public static boolean exists(String domain, String title)
+    {
+        if(getRequest(domain, title) == null)
+            return false;
+        return true;
+    }
+
+    public static void addElement(String domain,String sender, String details, String title)
+    {
+        JsonRequest jd = new JsonRequest();
+        jd.setRequestDomain(domain);
+        jd.setRequestDetails(details);
+        jd.setRequestSender(sender);
+        jd.setReqeustTitle(title);
+        listRequest.add(jd);
+        JsonController.writeArrayRequest(path, listRequest);
+    }
+
+    public static void updateList()
+    {
+        listRequest = JsonController.readArrayRequest(path);
+    }
+
+
+}
